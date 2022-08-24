be98bc330560048d6dda26a34765177522edfdf0
Jose Murilo Texeira <josemurilot@gmail.com>
2022-08-24 17:16:10 -0300

    "Criando validacao de campos unicos"

---
+++ src/main/java/br/com/treinaweb/ediaristas/core/enums/TipoUsuario.java
@@ -0,0 +1,15 @@
+
+package br.com.treinaweb.ediaristas.core.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum TipoUsuario {
+    CLIENTE (1),
+    DIARISTA (2),
+    ADMIN (3);
+
+    private Integer id;
+}
---
+++ src/main/java/br/com/treinaweb/ediaristas/web/services/WebUsuarioService.java
@@ -0,0 +1,105 @@
+package br.com.treinaweb.ediaristas.web.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.FieldError;
+
+import br.com.treinaweb.ediaristas.core.enums.TipoUsuario;
+import br.com.treinaweb.ediaristas.core.exceptions.SenhasNaoConferemException;
+import br.com.treinaweb.ediaristas.core.exceptions.UsuarioJaCadastradoException;
+import br.com.treinaweb.ediaristas.core.exceptions.UsuarioNaoEncontradoException;
+import br.com.treinaweb.ediaristas.core.models.Usuario;
+import br.com.treinaweb.ediaristas.core.repositories.UsuarioRepository;
+import br.com.treinaweb.ediaristas.web.dtos.UsuarioCadastroForm;
+import br.com.treinaweb.ediaristas.web.dtos.UsuarioEdicaoForm;
+import br.com.treinaweb.ediaristas.web.mappers.WebUsuarioMapper;
+
+@Service
+public class WebUsuarioService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Autowired
+    private WebUsuarioMapper mapper;
+
+         
+    public List<Usuario> buscarTodos() {
+        return repository.findAll();
+    }
+
+    public Usuario cadastrar(UsuarioCadastroForm form) {
+
+        var senha = form.getSenha();
+        var confirmacaoSenha = form.getConfirmacaoSenha();
+
+        if(!senha.equals(confirmacaoSenha)){
+            var mensagem = "Os dois campos de senha nao conferem!";
+            var fieldError =  new FieldError(form.getClass().getName(), "confirmacaoSenha", form.getConfirmacaoSenha(), false, null, null, mensagem);
+
+            throw new SenhasNaoConferemException(mensagem, fieldError);
+        }
+
+        repository.findByEmail(form.getEmail()).ifPresent((usuario) -> {
+            var mensagem = "Ja existe um usuario cadastrado com esse email!";
+            var fieldError =  new FieldError(form.getClass().getName(), "email", form.getEmail(), false, null, null, mensagem);
+            throw new UsuarioJaCadastradoException(mensagem, fieldError);
+        });
+        
+
+        var model = mapper.toModel(form);
+
+        
+        model.setTipoUsuario(TipoUsuario.ADMIN);
+        
+        validarCamposUnicos(model);
+        
+
+        return repository.save(model);
+    }
+
+    public Usuario buscarPorId(Long id) {
+        var mensagem = String.format("Usuário com ID %d não encontrado", id);
+
+        return repository.findById(id)
+        .orElseThrow(() -> new UsuarioNaoEncontradoException(mensagem));
+    }
+
+    public UsuarioEdicaoForm buscarFormPorId(Long id) {
+        var usuario = buscarPorId(id);
+
+        return mapper.toForm(usuario);
+    }
+
+    public Usuario editar(UsuarioEdicaoForm form, Long id) {
+        var usuario = buscarPorId(id);
+
+        var model = mapper.toModel(form);
+        model.setId(usuario.getId());
+        model.setSenha(usuario.getSenha());
+        model.setTipoUsuario(usuario.getTipoUsuario());
+
+        validarCamposUnicos(model);
+
+        
+
+        return repository.save(model);
+    }
+
+    public void excluirPorId(Long id) {
+        var usuario = buscarPorId(id);
+
+        repository.delete(usuario);
+    }
+
+    private void validarCamposUnicos(Usuario usuario) {
+        if (repository.isEmailJaCadastrado(usuario.getEmail(), usuario.getId())) {
+            var mensagem = "Já existe um usuário cadastrado com esse e-mail";
+            var fieldError = new FieldError(usuario.getClass().getName(), "email", usuario.getEmail(), false, null, null, mensagem);
+            throw new UsuarioJaCadastradoException(mensagem, fieldError);
+        }
+        
+    }
+}
